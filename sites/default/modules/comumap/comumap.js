6e58183cc0c42077745d91fb8ce056195c4a6ad1
Cesar Montalvo <cmontalvo@devsu.com>
2012-05-06 17:52:40 -0500

    Add comments to custom module

--- sites/default/modules/comumap/comumap.js
+++ sites/default/modules/comumap/comumap.js
@@ -2,8 +2,11 @@
 
   Drupal.behaviors.comumap = {
     attach: function(context, settings) {
+      //Ajax menu callback
       $('#nice-menu-1 a').click(function () {
+        //Build Url for ajax callback
         var load_url = '/comumap_callback?url=' + $(this).attr('href').replace('/', '');
+        //Ajax Callback
         $.ajax({
           url: load_url,
           type: "POST",
